7c476f118b7bd50e16ba05f0bb16a7828fdf1972
Rogério de Sá <rogeriodesaf@gmail.com>
2022-10-11 03:01:38 -0300

    Instalando o mongoose

---
+++ db/conn.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+async function main(){
+    await mongoose.connect('mongodb://localhost:27017/testemongoose')
+    console.log('Conectou ao MongoDB com Mongoose!')
+
+}
+
+main().catch((err)=> console.log(err))
+
+module.exports = mongoose
